1ff3d6d40b09508c48dce9586b49e9e530a78e5d
jwatkyns <jwatkyns@gmail.com>
2019-09-17 12:33:27 +0200

    Complete project upload

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwatkyn <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/28 08:39:57 by jwatkyn           #+#    #+#             */
+/*   Updated: 2018/06/05 13:55:51 by jwatkyn          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft.h"
+# include <fcntl.h>
+
+# define BUFF_SIZE 100
+
+int				get_next_line(const int fd, char **line);
+
+#endif
